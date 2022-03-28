916fafc7735196a986da4662962332bf8b3b3aca
cetherdev <robert.hct@gmail.com>
2022-03-28 15:49:28 -0600

    refactor: Cambio de tipografía y márgenes corregidos

--- views/formatos/fdp01.php
+++ views/formatos/fdp01.php
@@ -1,181 +1,186 @@
 <?php
-  require( "pdf.php" );
+require("pdf.php");
 
-  session_start( );
+session_start();
 
-  if(!isset($_GET["id"]) && !$_GET["id"]){
-    header("../home.php");
-  }
-
-  class PDP01 extends PDF{
-
-    public function nuevaPagina(){
-      $this->AliasNbPages( );
-      $this->AddPage( "P", "Letter" );
-    }
-
-    public function getNombreFormato(){
-      // Nombre del formato
-        $this->SetFont( "Arial", "B", 11 );
-        $this->Ln( 10);
-        $this->SetTextColor( 255, 255, 255 );
-        $this->SetFillColor( 0, 127, 204 );
-        $this->Cell( 140, 5, "", 0, 0, "L");
-        $this->Cell( 35, 6, "FDP01", 0, 0, "R", true);
-        $this->Ln( 10 );
-        $this->SetTextColor( 0, 0, 0 );
-    }
-    public function footer(){
-      $this->SetY(-20);
-      $this->SetFont("Arial", "I", 9);
-      $this->Cell( 0, 10, utf8_decode($this->PageNo()." de {nb}"), 0, 1, "R" );
-      $this->Image( "../../images/jalisco.png",20,245,20);
-
-    }
-    public function header(){
-      $this->Image("../../images/encabezado.jpg",0,15,75);
-      $this->Image("../../images/direccion_sicyt.PNG",155,12,40);
-
-    }
-  }
-
-  $pdf = new PDP01();
-  $pdf->getData($_GET["id"]);
-  $pdf->getDataPlantel($pdf->plantel["id"]);
-
-  $pdf->nuevaPagina();
-
-  $pdf->SetFont( "Arial", "B", 10 );
-  $pdf->SetMargins(20, 35 , 20);
-
-  $pdf->SetAutoPageBreak(true, 30);
-  //$pdf->Image( "../../images/encabezado.jpg",0,5,100);
-  // Nombre del formato
-  $pdf->Ln( 15 );
-  $pdf->getNombreFormato();
-
-  $pdf->SetTextColor( 0, 127, 204 );
-  $pdf->Cell( 0, 5, utf8_decode("FUNDAMENTACIÓN DEL PLAN DE ESTUDIOS "), 0, 1, "R");
-  $pdf->SetTextColor( 0, 0, 0 );
-  $pdf->Ln( 10 );
-  // echo "REPRESENTANTE: ";var_dump($pdf->usuarioR);
-  // echo "<br>SOLICITUD: ";var_dump($pdf->solicitud);
-  // exit();
-
-  $programa = $pdf->nivel["descripcion"]." en ".$pdf->programa["nombre"];
-  $modalidad = $pdf->modalidad["nombre"];
-  $periodo = $pdf->ciclo["nombre"];
+if (!isset($_GET["id"]) && !$_GET["id"]) {
+  header("../home.php");
+}
 
 
+$pdf = new PDF();
 
-  $pdf->SetFont( "Arial", "B", 11 );
-  if ($pdf->nombreInstitucion) {
-    $pdf->Cell( 0, 5,utf8_decode($pdf->nombreInstitucion), 0,1, "L");
-  } else if($pdf->nombrePropuesto){
-    $pdf->Cell( 0, 5,utf8_decode($pdf->nombrePropuesto["nombre_propuesto1"]), 0,1, "L");
-  }
-  $pdf->Cell( 0, 5,utf8_decode(mb_strtoupper($programa.", ".$modalidad)), 0,1, "L");
-  $pdf->Ln( 5 );
+$pdf->getData($_GET["id"]);
+$pdf->getDataPlantel($pdf->plantel["id"]);
+$pdf->AliasNbPages();
 
-  $pdf->SetFillColor( 166, 166, 166 );
-  $pdf->SetFont( "Arial", "B", 9 );
-  $pdf->Cell( 0, 5, utf8_decode("1.1  ESTUDIO DE PERTINENCIA Y FACTIBILIDAD"), 1, 1, "C", true );
-  $pdf->SetFillColor( 191, 191, 191 );
-  $pdf->Cell( 0, 5, utf8_decode("CON REFERENCIA GENERAL"), 1, 1, "L", true );
-  $pdf->SetFont( "Arial", "", 9 );
-  $pdf->MultiCell( 0, 5, utf8_decode($pdf->programa["necesidad_social"]), 1, "J");
-  if($pdf->checkNewPage()){
-  $pdf->Ln(15);
-  }
-  $pdf->SetFont( "Arial", "B", 9 );
-  $pdf->Cell( 0, 5, utf8_decode("CON REFERENCIA AL PERFIL DE EGRESO"), 1, 1, "L", true );
-  $pdf->SetFont( "Arial", "", 9 );
-  $pdf->MultiCell( 0, 5, utf8_decode($pdf->programa["necesidad_profesional"]), 1, "J");
-  if($pdf->checkNewPage()){
-  $pdf->Ln(15);
-  }
-  $pdf->SetFont( "Arial", "B", 9 );
-  $pdf->Cell( 0, 5, utf8_decode("CON REFERENCIA AL PERFIL DE NUEVO INGRESO"), 1, 1, "L", true );
-  $pdf->SetFont( "Arial", "", 9 );
-  $pdf->MultiCell( 0, 5, utf8_decode($pdf->programa["necesidad_institucional"]), 1, "J");
-  if($pdf->checkNewPage()){
-  $pdf->Ln(15);
-  }
-  $pdf->Ln(5);
-
-  $pdf->SetFillColor( 166, 166, 166 );
-  $pdf->SetFont( "Arial", "B", 9 );
-  $pdf->Cell( 0, 5, utf8_decode("1.2. ESTUDIO DE OFERTA Y DEMANDA"), 1, 1, "C", true );
-  $pdf->SetFont( "Arial", "", 9 );
-  $pdf->MultiCell( 0, 5, utf8_decode($pdf->programa["estudio_oferta_demanda"]), 1, "J");
-  if($pdf->checkNewPage()){
-  $pdf->Ln(15);
-  }
-  $pdf->Ln(5);
-
-  $pdf->SetFillColor( 166, 166, 166 );
-  $pdf->SetFont( "Arial", "B", 9 );
-  $pdf->Cell( 0, 5, utf8_decode("1.3 FUENTES DE INFORMACIÓN"), 1, 1, "C", true );
-  $pdf->SetFont( "Arial", "", 9 );
-  $pdf->MultiCell( 0, 5, utf8_decode($pdf->programa["fuentes_informacion"]), 1, "L");
-  if($pdf->checkNewPage()){
-  $pdf->Ln(15);
-  }
-
-  $pdf->Ln(5);
-  $pdf->SetFillColor( 166, 166, 166 );
-  $pdf->SetFont( "Arial", "B", 9 );
-  $pdf->Cell( 0, 5, utf8_decode("1.4 IDEARIO INSTITUCIONAL"), 1, 1, "C", true );
-  $pdf->SetFillColor( 191, 191, 191 );
-  $pdf->Cell( 0, 5, utf8_decode("MISIÓN"), 1, 1, "L", true );
-  $pdf->SetFont( "Arial", "", 9 );
-  $pdf->MultiCell( 0, 5, utf8_decode($pdf->institucion["mision"]), 1, "J");
-  if($pdf->checkNewPage()){
-  $pdf->Ln(15);
-  }
-
-  $pdf->SetFont( "Arial", "B", 9 );
-  $pdf->SetFillColor( 191, 191, 191 );
-  $pdf->Cell( 0, 5, utf8_decode("VISIÓN"), 1, 1, "L", true );
-  $pdf->SetFont( "Arial", "", 9 );
-  $pdf->MultiCell( 0, 5, utf8_decode($pdf->institucion["vision"]), 1, "J");
-  if($pdf->checkNewPage()){
-  $pdf->Ln(15);
-  }
-
-  $pdf->SetFont( "Arial", "B", 9 );
-  $pdf->SetFillColor( 191, 191, 191 );
-  $pdf->Cell( 0, 5, utf8_decode("VALORES INSTITUCIONALES"), 1, 1, "L", true );
-  $pdf->SetFont( "Arial", "", 9 );
-  $pdf->MultiCell( 0, 5, utf8_decode($pdf->institucion["valores_institucionales"]), 1, "J");
-  if($pdf->checkNewPage()){
-  $pdf->Ln(15);
-  }
-
-  $pdf->SetFont( "Arial", "B", 9 );
-  $pdf->Cell( 0, 5, utf8_decode("HISTORIA"), 1, 1, "L", true );
-  $pdf->SetFont( "Arial", "", 9 );
-  $pdf->MultiCell( 0, 5, utf8_decode($pdf->institucion["historia"]), 1, "J");
-  if($pdf->checkNewPage()){
-  $pdf->Ln(15);
-  }
-  $pdf->Ln(5);
-
-  $pdf->SetFillColor( 166, 166, 166 );
-  $pdf->SetFont( "Arial", "B", 9 );
-  $pdf->Cell( 0, 5, utf8_decode("1.5 RECURSOS PARA SU OPERACIÓN"), 1, 1, "C", true );
-  $pdf->SetFont( "Arial", "", 9 );
-  $pdf->MultiCell( 0, 5, utf8_decode($pdf->programa["recursos_operacion"]), 1, "J");
-  if($pdf->checkNewPage()){
-  $pdf->Ln(15);
-  }
-
-  $pdf->Ln( 30 );
-  $pdf->SetFont( "Arial", "B", 11 );
-  $pdf->Cell( 0, 5, "BAJO PROTESTA DE DECIR VERDAD", 0, 0, "C");
-  $pdf->Ln( 10);
-
-  $pdf->Cell( 0, 5, utf8_decode(mb_strtoupper($pdf->nombreRepresentante)), 0, 0, "C");
+$pdf->AddPage("P", "Letter");
+$pdf->SetMargins(20, 35, 20);
+$pdf->SetAutoPageBreak(true, 30);
 
 
-  $pdf->Output( "I", "PDP01.pdf" );
+// Nombre del formato
+$pdf->SetFont("Nutmegb", "", 11);
+$pdf->Ln(25);
+$x = $pdf->SetX(20);
+$y = $pdf->SetY(35);
+$pdf->SetTextColor(255, 255, 255);
+$pdf->SetFillColor(0, 127, 204);
+$pdf->Cell(140, 5, "", 0, 0, "L");
+
+$pdf->Cell(35, 6, "FDP01", 0, 0, "R", true);
+$pdf->Ln(10);
+
+$pdf->SetTextColor(0, 127, 204);
+$pdf->Cell(0, 5, utf8_decode("FUNDAMENTACIÓN DEL PLAN DE ESTUDIOS"), 0, 1, "L");
+$pdf->Ln(5);
+$pdf->SetTextColor(0, 0, 0);
+
+//Datos del plan de estudios
+$pdf->SetFont("Nutmeg", "", 9);
+
+if ($pdf->institucion["es_nombre_autorizado"]) {
+  $dataPlanEstudios = array(
+    [
+      "name" => utf8_decode("NOMBRE DE LA INSTITUCIÓN"),
+      "description" => utf8_decode(mb_strtoupper($pdf->nombreInstitucion))
+    ],
+    [
+      "name" => utf8_decode("NIVEL Y NOMBRE DEL PLAN DE ESTUDIOS"),
+      "description" => utf8_decode(mb_strtoupper($pdf->nivel["descripcion"] . " EN " . $pdf->programa["nombre"]))
+    ],
+    [
+      "name" => utf8_decode("MODALIDAD"),
+      "description" => utf8_decode(mb_strtoupper($pdf->modalidad["nombre"]))
+    ],
+    [
+      "name" => utf8_decode("DURACIÓN DEL PROGRAMA"),
+      "description" => utf8_decode(mb_strtoupper($pdf->programa["duracion"]))
+    ],
+  );
+} else {
+  $dataPlanEstudios = array(
+    [
+      "name" => utf8_decode("NIVEL Y NOMBRE DEL PLAN DE ESTUDIOS"),
+      "description" => utf8_decode(mb_strtoupper($pdf->nivel["descripcion"] . " EN " . $pdf->programa["nombre"]))
+    ],
+    [
+      "name" => utf8_decode("MODALIDAD"),
+      "description" => utf8_decode(mb_strtoupper($pdf->modalidad["nombre"]))
+    ],
+    [
+      "name" => utf8_decode("DURACIÓN DEL PROGRAMA"),
+      "description" => utf8_decode(mb_strtoupper($pdf->programa["duracion"]))
+    ],
+  );
+}
+
+//set widht for each column (6 columns)
+$pdf->SetWidths(array(80, 94));
+
+//set line height
+$pdf->SetLineHeight(5);
+
+$pdf->SetColors([[191, 191, 191], []]);
+
+foreach ($dataPlanEstudios as $item) {
+  // write data using Row() method containing array of values
+  $pdf->Row(array(
+    $item['name'],
+    $item['description']
+  ));
+}
+$pdf->Ln();
+$pdf->Ln();
+
+$pdf->SetFillColor(166, 166, 166);
+$pdf->SetFont("Nutmegb", "", 9);
+$pdf->Cell(0, 5, utf8_decode("1. ESTUDIO DE PERTINENCIA Y FACTIBILIDAD"), 1, 1, "C", true);
+$pdf->SetFillColor(191, 191, 191);
+$pdf->Cell(0, 5, utf8_decode("CON REFERENCIA GENERAL"), 1, 1, "L", true);
+$pdf->SetFont("Nutmeg", "", 9);
+$pdf->MultiCell(0, 5, utf8_decode($pdf->programa["necesidad_social"]), 0, "J");
+$pdf->SetFont("Nutmegb", "", 9);
+$pdf->Cell(0, 5, utf8_decode("CON REFERENCIA AL PERFIL DE EGRESO"), 1, 1, "L", true);
+$pdf->SetFont("Nutmeg", "", 9);
+$pdf->MultiCell(0, 5, utf8_decode($pdf->programa["necesidad_profesional"]), 0, "J");
+$pdf->ln();
+$pdf->SetFont("Nutmegb", "", 9);
+$pdf->Cell(0, 5, utf8_decode("CON REFERENCIA AL PERFIL DE NUEVO INGRESO"), 1, 1, "L", true);
+$pdf->SetFont("Nutmeg", "", 9);
+$pdf->MultiCell(0, 5, utf8_decode($pdf->programa["necesidad_institucional"]), 0, "J");
+
+$pdf->SetFillColor(166, 166, 166);
+$pdf->SetFont("Nutmegb", "", 9);
+$pdf->Cell(0, 5, utf8_decode("2. ESTUDIO DE OFERTA Y DEMANDA"), 1, 1, "C", true);
+$pdf->SetFont("Nutmeg", "", 9);
+$pdf->MultiCell(0, 5, utf8_decode($pdf->programa["estudio_oferta_demanda"]), 0, "J");
+
+$pdf->ln();
+
+$pdf->SetFillColor(166, 166, 166);
+$pdf->SetFont("Nutmegb", "", 9);
+$pdf->Cell(0, 5, utf8_decode("3 FUENTES DE INFORMACIÓN"), 1, 1, "C", true);
+$pdf->SetFont("Nutmeg", "", 9);
+$pdf->MultiCell(0, 5, utf8_decode($pdf->programa["fuentes_informacion"]), 0, "L");
+
+$pdf->ln();
+
+$pdf->SetFillColor(166, 166, 166);
+$pdf->SetFont("Nutmegb", "", 9);
+$pdf->Cell(0, 5, utf8_decode("4 MODELO EDUCATIVO"), 1, 1, "C", true);
+$pdf->SetFont("Nutmeg", "", 9);
+$pdf->MultiCell(0, 5, utf8_decode(""), 0, "L");
+
+$pdf->ln();
+
+$pdf->SetFillColor(166, 166, 166);
+$pdf->SetFont("Nutmegb", "", 9);
+$pdf->Cell(0, 5, utf8_decode("5 POLÍTICAS DE FUNCIONAMIENTO"), 1, 1, "C", true);
+$pdf->SetFont("Nutmeg", "", 9);
+$pdf->MultiCell(0, 5, utf8_decode(""), 0, "L");
+
+$pdf->ln();
+
+$pdf->SetFillColor(166, 166, 166);
+$pdf->SetFont("Nutmegb", "", 9);
+$pdf->Cell(0, 5, utf8_decode("6 IDEARIO INSTITUCIONAL"), 1, 1, "C", true);
+$pdf->SetFillColor(191, 191, 191);
+$pdf->Cell(0, 5, utf8_decode("MISIÓN"), 1, 1, "L", true);
+$pdf->SetFont("Nutmeg", "", 9);
+$pdf->MultiCell(0, 5, utf8_decode($pdf->institucion["mision"]), 0, "J");
+$pdf->ln();
+
+$pdf->SetFont("Nutmegb", "", 9);
+$pdf->SetFillColor(191, 191, 191);
+$pdf->Cell(0, 5, utf8_decode("VISIÓN"), 1, 1, "L", true);
+$pdf->SetFont("Nutmeg", "", 9);
+$pdf->MultiCell(0, 5, utf8_decode($pdf->institucion["vision"]), 0, "J");
+$pdf->ln();
+
+$pdf->SetFont("Nutmegb", "", 9);
+$pdf->SetFillColor(191, 191, 191);
+$pdf->Cell(0, 5, utf8_decode("VALORES INSTITUCIONALES"), 1, 1, "L", true);
+$pdf->SetFont("Nutmeg", "", 9);
+$pdf->MultiCell(0, 5, utf8_decode($pdf->institucion["valores_institucionales"]), 0, "J");
+
+$pdf->ln();
+
+
+$pdf->SetFont("Nutmegb", "", 9);
+$pdf->Cell(0, 5, utf8_decode("HISTORIA"), 1, 1, "L", true);
+$pdf->SetFont("Nutmeg", "", 9);
+$pdf->MultiCell(0, 5, utf8_decode($pdf->institucion["historia"]), 0, "J");
+
+
+$pdf->Ln(30);
+
+$pdf->SetTextColor(0, 0, 0);
+$pdf->SetFont("Nutmeg", "", 11);
+$pdf->Cell(0, 5, utf8_decode("BAJO PROTESTA DE DECIR VERDAD"), 0, 1, "C");
+$pdf->SetFont("Nutmegb", "", 11);
+$pdf->Cell(0, 5, utf8_decode(mb_strtoupper($pdf->nombreRepresentante)), 0, 1, "C");
+
+
+
+$pdf->Output("I", "PDP01.pdf");
