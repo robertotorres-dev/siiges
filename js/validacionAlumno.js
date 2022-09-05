4afa75a34bdc043a7d9d0f088eca5a988eeada4b
cetherdev <robert.hct@gmail.com>
2022-09-05 10:00:25 -0500

    feat: control total y autonomía del módulo de validación

--- js/validacionAlumno.js
+++ js/validacionAlumno.js
@@ -15,37 +15,108 @@ ValidacionAlumno.normalize = function () {
       if (mapping.hasOwnProperty(str.charAt(i))) ret.push(mapping[c]);
       else ret.push(c);
     }
-    return ret.join( '' ).toUpperCase();
+    return ret.join('').toUpperCase();
   };
 };
 
+ValidacionAlumno.guardarCambios = async function (e) {
+  if (e) {
+    e.preventDefault();
+  }
 
-$gmx(document).ready(function() {
+  const formData = new FormData(document.getElementById('form1'));
+  console.log(formData.get('url'));
+  if (!formData.get('url')) {
+    formData.append('url', `../views/ce-validacion.php?programa_id=${formData.get('programa_id')}&codigo=200`)
+  }
+  formData.append('webService', 'guardar')
+  formData.append('estatus', '1')
+  const ajaxPath = "../controllers/control-validacion.php";
+
+  try {
+    const response = await fetch(
+      ajaxPath,
+      {
+        method: "post",
+        body: formData,
+        redirect: 'follow'
+      });
+    if (response.ok) {
+      if (response.redirected) {
+        window.location.href = response.url;
+      }
+      const data = await response.json();
+    }
+  } catch (err) {
+    console.error(err instanceof SyntaxError);
+    console.error(err.message);
+    console.error(err.stack);
+  }
+
+
+}
+
+ValidacionAlumno.habilitarCaptura = async function (e) {
+  if (e) {
+    e.preventDefault();
+  }
+  const formData = new FormData(document.getElementById('form1'));
+  console.log(formData.get('url'));
+  if (!formData.get('url')) {
+    formData.append('url', `../views/ce-validacion.php?programa_id=${formData.get('programa_id')}&codigo=201`)
+  }
+  formData.append('webService', 'habilitarCaptura')
+  formData.append('estatus', '0')
+  const ajaxPath = "../controllers/control-validacion.php";
+
+  try {
+    const response = await fetch(
+      ajaxPath,
+      {
+        method: "post",
+        body: formData,
+        redirect: 'follow'
+      });
+    if (response.ok) {
+      if (response.redirected) {
+        window.location.href = response.url;
+      }
+      const data = await response.json();
+    }
+  } catch (err) {
+    console.error(err instanceof SyntaxError);
+    console.error(err.message);
+    console.error(err.stack);
+  }
+}
+
+
+$gmx(document).ready(function () {
 
   $('[data-toggle="tooltip"]').tooltip();
 
   $("#fecha_expedicion").datepicker({
     firstDay: 1,
-    monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
-    dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+    monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+    dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'],
     dateFormat: 'yy-mm-dd'
   })
   $("#fecha_validacion").datepicker({
     firstDay: 1,
-    monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
-    dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+    monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+    dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'],
     dateFormat: 'yy-mm-dd'
   })
   $("#fecha_inicio_antecedente").datepicker({
     firstDay: 1,
-    monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
-    dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+    monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+    dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'],
     dateFormat: 'yy-mm-dd'
   })
   $("#fecha_fin_antecedente").datepicker({
     firstDay: 1,
-    monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
-    dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+    monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+    dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'],
     dateFormat: 'yy-mm-dd'
   })
 
@@ -57,17 +128,25 @@ $gmx(document).ready(function() {
         let height = image.offsetHeight;
         let mouseX = e.offsetX;
         let mouseY = e.offsetY;
-        
+
         let bgPosX = (mouseX / width * 100);
         let bgPosY = (mouseY / height * 100);
-        
+
         image.style.backgroundPosition = `${bgPosX}% ${bgPosY}%`;
-        });
-    
-        image.addEventListener('mouseleave', function () {
+      });
+
+      image.addEventListener('mouseleave', function () {
         image.style.backgroundPosition = "center";
       });
     });
   }
+
+
+  guardarCambiosButton = document.getElementById('guardarCambios')
+  habilitarCapturaButton = document.getElementById('habilitarCaptura')
+
+  guardarCambiosButton && guardarCambiosButton.addEventListener('click', ValidacionAlumno.guardarCambios)
+  habilitarCapturaButton && habilitarCaptura.addEventListener('click', ValidacionAlumno.habilitarCaptura)
+
 
 });
