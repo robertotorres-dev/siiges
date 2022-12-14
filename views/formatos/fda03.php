208d259492c30be28fb7028c298c0dae1bce7cfa
cetherdev <robert.hct@gmail.com>
2022-12-13 23:54:54 -0600

    SDT-12: delete location

--- views/formatos/fda03.php
+++ views/formatos/fda03.php
@@ -42,7 +42,7 @@ $pdf->Ln(5);
 // Fecha
 $pdf->SetFont("Nutmeg", "", 9);
 $fecha =  $pdf->fecha;
-$pdf->Cell(0, 5, utf8_decode(mb_strtoupper("Guadalajara, Jal. a $fecha")), 0, 1, "R");
+$pdf->Cell(0, 5, utf8_decode(mb_strtoupper($fecha)), 0, 1, "R");
 $pdf->Ln(5);
 
 
