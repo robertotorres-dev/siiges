1ad0f7ab60c75e0e032d84710fbac5d4c882f31f
Lau1980 <laura.jaramillo@jalisco.gob.mx>
2022-12-26 11:43:04 -0600

    cambios en archivo detalles

--- views/detalles-solicitudes.php
+++ views/detalles-solicitudes.php
@@ -1,7 +1,7 @@
 <?php
 // Válida los permisos del usuario de la sesión
-require_once ".. /utilidades/utileria-general.php";
-require_once ".. /models/modelo-rol.php";
+require_once "../utilities/utileria-general.php";
+require_once "../models/modelo-rol.php";
 Utileria::validarSesion(basename(__FILE__));
 //====================================================================================================
 $resultado = "";
@@ -10,30 +10,30 @@ if (isset($_SESSION["resultado"])) {
 	unset($_SESSION["resultado"]);
 }
 ?>
-<! DOCTYPE html>
+<!DOCTYPE html>
 <html>
 
-<cabeza>
+<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Detalles del proceso</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/css/bootstrap-select.min.css">
 
-	<!-- CSS GOB. MX -->
-	<enlace href=".. /favicon.ico" rel="icono de acceso directo">
+	<!-- CSS GOB.MX -->
+	<link href="../favicon.ico" rel="shortcut icon">
 	<link href="https://framework-gb.cdn.gob.mx/assets/styles/main.css" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href=".. /css/estilos.css">
-</cabeza>
+	<link rel="stylesheet" type="text/css" href="../css/estilos.css">
+</head>
 
-<Clase de cuerpo ="">
+<body class="">
 
 	<!-- HEADER Y BARRA DE NAVEGACION -->
-	<?php require_once "menú.php"; ?>
+	<?php require_once "menu.php"; ?>
 
 	<!-- CUERPO DEL FORMULARIO -->
-	<div class="contenedor">
-		<section class="main row margin-section-formularios ">
+	<div class="container">
+		<section class="main row margin-section-formularios">
 			<div class="col-sm-12 col-md-12">
 				<!-- BARRA DE USUARIO -->
 				<ol class="breadcrumb pull-right">
@@ -49,25 +49,25 @@ if (isset($_SESSION["resultado"])) {
 					<?php } else { ?>
 						<li><a href="solicitudes.php">Solicitudes</a></li>
 					<?php } ?>
-					<li class="active">Detalles del proceso</li >
-				</Ol>
+					<li class="active">Detalles del proceso</li>
+				</ol>
 
 				<h2>Detalles de la solicitud</h2>
-				<hr class="rojo">
+				<hr class="red">
 				<div id="cargando" class="loader">
 
-				</Div>
+				</div>
 				<div id="mensaje"></div>
 				<!-- INICIA FORMULARIO -->
-				<form class="form-horizontal" action="" enctype="multipart/form-data" method="post ">
+				<form class="form-horizontal" action="" enctype="multipart/form-data" method="post">
 					<!-- Tipo de trámite -->
 					<div id="tipo-tramite" class="form-group">
 						<div class="col-sm-12 col-md-3">
-							<label class="control-label" for="">Tipo de trámite</label><br >
+							<label class="control-label" for="">Tipo de trámite</label><br>
 							<select id="tipo_solicitud" class="form-control" readonly>
-								<option value="">Seleccione una opción</option >
-							</escoger>
-						</Div>
+								<option value="">Seleccione una opción</option>
+							</select>
+						</div>
 						<input type="hidden" id="tipo_control">
 						<div class="col-sm-12 col-md-3">
 							<label class="control-label">Fecha de recepción</label>
@@ -184,10 +184,6 @@ if (isset($_SESSION["resultado"])) {
 							<h4>Observaciones del proceso</h4>
 							<textarea id="observaciones" class="form-control" name="name" rows="8" cols="80" readonly></textarea>
 						</div>
-						<!-- <div class="col-sm-12 col-md-5">
-								<label class="control-label" for="">Fecha limite para atender observaciones</label><br>
-								<input type="text" id="" name="" class="form-control" value="" placeholder="Fecha" readonly>
-							</div> -->
 					</div>
 				</form>
 				<form class="form-horizontal" action="../controllers/contoller.programa" method="post">
@@ -196,7 +192,6 @@ if (isset($_SESSION["resultado"])) {
 				</form>
 			</div>
 		</section>
-
 
 		<div class="container">
 			<section class="main row margin-section-formularios">
@@ -225,28 +220,26 @@ if (isset($_SESSION["resultado"])) {
 						<a target="_blank" href=<?= "formatos/fdp06.php?id=" . $_GET["solicitud"] ?>>FDP 06</a><br>
 						<a target="_blank" href=<?= "formatos/fdp07.php?id=" . $_GET["solicitud"] ?>>FDP 07</a><br>
 						<a target="_blank" id="fdp08"></a><br>
-					</Div>
+					</div>
 					<?php if (Rol::ROL_REPRESENTANTE_LEGAL == $_SESSION["rol_id"] || Rol::ROL_GESTOR == $_SESSION["rol_id"]) : ?>
 						<div class="form-group col-sm-6 col-md-4">
 							<h4>Documentos</h4>
-							<a target="_blank" href=<?= "oficios/orden-inspeccion.php?id=" . $_GET["solicitud"] ?> id="OrdenInspección" name="<?= $_GET["solicitud"] ?>">Orden de Inspección</a><br >
-							<!-- <a target="_blank" href="oficios/orden-inspeccion.php" id="OrdenInspección" name="<?= $_GET["solicitud"] ?>" class="get">Orden de Inspección</a><br> -->
-							<!-- <a target="_blank" href="oficios/observaciones.php" id="Observaciones" name="<?= $_GET["solicitud"] ?>" class="get">Observaciones</a><br> -->
-						</Div>
+							<a target="_blank" href=<?= "oficios/orden-inspeccion.php?id=" . $_GET["solicitud"] ?> id="OrdenInspección" name="<?= $_GET["solicitud"] ?>">Orden de Inspección</a><br>
+						</div>
 					<?php endif; ?>
 
 					<?php if (Rol::ROL_ADMIN == $_SESSION["rol_id"] || Rol::ROL_SICYT_EDITAR == $_SESSION["rol_id"]) : ?>
 						<div class="form-group col-sm-6 col-md-4" id="rvoe">
 							<h4>RVOE</h4>
-							<a target="_blank" href="oficios/acuerdo-cambio-domicilio.php" id="AcuerdoCambioDomicilio" name="<?= $_GET["solicitud"] ?>" class="get" hidden>Acuerdo de Cambio de Domicilio</a>< Br>
+							<a target="_blank" href="oficios/acuerdo-cambio-domicilio.php" id="AcuerdoCambioDomicilio" name="<?= $_GET["solicitud"] ?>" class="get" hidden>Acuerdo de Cambio de Domicilio</a><br>
 
-							<a target="_blank" href="oficios/acuerdo-cambio-representante-legal.php" id="AcuerdoCambioRepresentanteLegal" name="<?= $_GET["solicitud"] ?>" class="post" hidden>Acuerdo de Cambio de Representante Legal</a> <br>
+							<a target="_blank" href="oficios/acuerdo-cambio-representante-legal.php" id="AcuerdoCambioRepresentanteLegal" name="<?= $_GET["solicitud"] ?>" class="post" hidden>Acuerdo de Cambio de Representante Legal</a><br>
 
 							<a target="_blank" href="oficios/refrendo-rvoe.php" id="RefrendoRVOE" name="<?= $_GET["solicitud"] ?>" class="get" hidden>Refrendo de RVOE</a><br>
 
-							<a target="_blank" href="oficios/acuerdo-rvoe.php" id="AcuerdoRVOE" name="<?= $_GET["solicitud"] ?>" class="get" hidden>Acuerdo de RVOE</a><br> <br><br>
+							<a target="_blank" href="oficios/acuerdo-rvoe.php" id="AcuerdoRVOE" name="<?= $_GET["solicitud"] ?>" class="get" hidden>Acuerdo de RVOE</a><br><br><br>
 
-						</Div>
+						</div>
 
 
 						<div class="form-group col-sm-6 col-md-4">
@@ -254,171 +247,171 @@ if (isset($_SESSION["resultado"])) {
 							<a target="_blank" href=<?= "dictamenes/carta-aceptacion.php?id=" . $_GET["solicitud"] ?> id="CartaAceptacion">Carta de Aceptación</a><br>
 							<a target="_blank" href=<?= "dictamenes/carta-asignacion-evaluador.php?id=" . $_GET["solicitud"] ?> id="CartaAsignacion">Carta de Asignación de Evaluador</a><br>
 							<a target="_blank" href=<?= "dictamenes/carta-imp-con.php?id=" . $_GET["solicitud"] ?> id="CartaImpCon">Carta de Imparcialidad y Confidencialidad</a><br>
-						</Div>
+						</div>
 
 						<div class="form-group col-sm-6 col-md-4">
 							<h4>Inspección</h4>
-							<a target="_blank" href=<?= "oficios/orden-inspeccion.php?id=" . $_GET["solicitud"] ?> id="OrdenInspección" name="<?= $_GET["solicitud"] ?>">Orden de Inspección</a><br >
-							<a target="_blank" href="oficios/acta-inspeccion.php" id="ActaDeInspeccion" name="<?= $_GET["solicitud"] ?>" class="post">Acta de Inspección</a><br >
-							<a target="_blank" href="oficios/acta-cierre.php" id="ActaDeCierre" name="<?= $_GET["solicitud"] ?>" class="post">Acta de Cierre</a><br >
-						</Div>
+							<a target="_blank" href=<?= "oficios/orden-inspeccion.php?id=" . $_GET["solicitud"] ?> id="OrdenInspección" name="<?= $_GET["solicitud"] ?>">Orden de Inspección</a><br>
+							<a target="_blank" href="oficios/acta-inspeccion.php" id="ActaDeInspeccion" name="<?= $_GET["solicitud"] ?>" class="post">Acta de Inspección</a><br>
+							<a target="_blank" href="oficios/acta-cierre.php" id="ActaDeCierre" name="<?= $_GET["solicitud"] ?>" class="post">Acta de Cierre</a><br>
+						</div>
 
 						<div class="form-group col-sm-6 col-md-4">
 							<h4>Otros</h4>
 							<a target="_blank" href=<?= "oficios/admisorio.php?id=" . $_GET["solicitud"] ?> id="Admisorio" name="<?= $_GET["solicitud"] ?>" class="post">Oficio Admisorio</a><br>
-							<a target="_blank" href="oficios/desistimiento.php" id="Desistimiento" name="<?= $_GET["solicitud"] ?>" class="post">Desistimiento</a><br >
+							<a target="_blank" href="oficios/desistimiento.php" id="Desistimiento" name="<?= $_GET["solicitud"] ?>" class="post">Desistimiento</a><br>
 
-						</Div>
+						</div>
 					<?php endif; ?>
-				</Div>
-			</sección>
-		</Div>
-	</Div>
+				</div>
+			</section>
+		</div>
+	</div>
 
-	<!-- Modal Oficio - >
-	<div id="modalOficio" class="fundido modal">
+	<!-- Modal Oficio-->
+	<div id="modalOficio" class="modal fade">
 		<div class="modal-dialog">
 			<div class="modal-content">
 				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times; </botón>
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 					<h4 class="modal-title">Oficio</h4>
-				</Div>
+				</div>
 				<div class="modal-body">
 					<p>Ingrese el número de oficio para generar este documento</p>
-					<label for="" class="control-label">Número de oficio</label >
+					<label for="" class="control-label">Número de oficio</label>
 					<input type="text" class="form-control" id="modal-numero-oficio" required>
-					<label for="" class="control-label">Fecha en que surte efecto</label >
+					<label for="" class="control-label">Fecha en que surte efecto</label>
 					<input type="date" class="form-control" id="modal-fecha_surte_efecto" required>
 
-				</Div>
+				</div>
 				<div class="modal-footer">
 					<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
 					<button id="modal-aceptar" type="button" class="btn btn-danger" data-dismiss="modal">Aceptar</button>
-				</Div>
-			</Div>
-		</Div>
+				</div>
+			</div>
+		</div>
 		<input type="hidden" id="modal-solicitud-id">
 		<input type="hidden" id="modal-enlace">
-	</Div>
+	</div>
 
 	<!-- Modal Acta de Cierre-->
-	<div id="modalActaDeCierre" class="fundido modal">
+	<div id="modalActaDeCierre" class="modal fade">
 		<div class="modal-dialog">
 			<div class="modal-content">
 				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times; </botón>
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 					<h4 class="modal-title">Acta de Cierre</h4>
-				</Div>
+				</div>
 				<form class="form-horizontal" id="form-acta-cierre" action="oficios/acta-cierre.php" enctype="multipart/form-data" method="post" target="_blank">
 					<div class="modal-body">
 						<p>Ingrese Observaciones para generar este documento</p>
-						<label for="" class="control-label">Observaciones</label >
+						<label for="" class="control-label">Observaciones</label>
 						<textarea class="form-control" id="modal-acta-cierre-observaciones" name="observaciones" required maxlength="255"></textarea>
-					</Div>
+					</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
 						<button id="modal-acta-cierre-aceptar" type="button" class="btn btn-danger" data-dismiss="modal">Aceptar</button>
-					</Div>
+					</div>
 					<input type="hidden" id="modal-acta-cierre-solicitud-id" name="id">
-				</forma>
-			</Div>
-		</Div>
-	</Div>
+				</form>
+			</div>
+		</div>
+	</div>
 
 	<!-- Modal Acta de Inspeccion-->
-	<div id="modalActaDeInspeccion" class="fundido modal">
+	<div id="modalActaDeInspeccion" class="modal fade">
 		<div class="modal-dialog">
 			<div class="modal-content">
 				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times; </botón>
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 					<h4 class="modal-title">Acta de Inspección</h4>
-				</Div>
+				</div>
 				<form class="form-horizontal" id="form-acta-inspeccion" action="oficios/acta-inspeccion.php" enctype="multipart/form-data" method="post" target="_blank">
 					<div class="modal-body">
 						<p>Ingrese la información requerida para generar este documento</p>
 						<p>Se le otorga la palabra al 'PARTICULAR'</p>
-						<label for="" class="control-label">Respuesta del particular</label >
+						<label for="" class="control-label">Respuesta del particular</label>
 						<textarea class="form-control" name="respuesta_particular" required maxlength="255"></textarea>
-					</Div>
+					</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
 						<button id="modal-acta-inspeccion-aceptar" type="button" class="btn btn-danger" data-dismiss="modal">Aceptar</button>
-					</Div>
+					</div>
 					<input type="hidden" id="modal-acta-inspeccion-solicitud-id" name="id">
-				</forma>
-			</Div>
-		</Div>
-	</Div>
+				</form>
+			</div>
+		</div>
+	</div>
 
-	<!-- Desistimiento modal-->
+	<!-- Modal Desistimiento-->
 	<div id="modalDesistimiento" class="modal fade">
 		<div class="modal-dialog">
 			<div class="modal-content">
 				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times; </botón>
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 					<h4 class="modal-title">Desistimiento</h4>
-				</Div>
+				</div>
 				<form class="form-horizontal" id="form-desistimiento" action="oficios/desistimiento.php" enctype="multipart/form-data" method="post" target="_blank">
 					<div class="modal-body">
 						<p>Ingrese el número de oficio para generar este documento</p>
-						<label for="" class="control-label">Número de oficio</label >
+						<label for="" class="control-label">Número de oficio</label>
 						<input type="text" class="form-control" id="modal-desistimiento-oficio" name="oficio" required>
-						<label for="" class="control-label">Consideraciones</label >
+						<label for="" class="control-label">Consideraciones</label>
 						<textarea class="form-control" name="consideraciones" required maxlength="255"></textarea>
-						<label for="" class="control-label">Resolución</label >
+						<label for="" class="control-label">Resolución</label>
 						<textarea class="form-control" name="resolucion" required maxlength="255"></textarea>
-					</Div>
+					</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
 						<button id="modal-desistimiento-aceptar" type="button" class="btn btn-danger" data-dismiss="modal">Aceptar</button>
-					</Div>
+					</div>
 					<input type="hidden" id="modal-desistimiento-solicitud-id" name="id">
-				</forma>
-			</Div>
-		</Div>
-	</Div>
+				</form>
+			</div>
+		</div>
+	</div>
 
 	<!-- Modal Acuerdo cambio de representante legal-->
-	<div id="modalAcuerdoCambioRepresentante" class="fundido modal">
+	<div id="modalAcuerdoCambioRepresentante" class="modal fade">
 		<div class="modal-dialog">
 			<div class="modal-content">
 				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times; </botón>
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 					<h4 class="modal-title">Acuero de Cambio de Representante Legal</h4>
-				</Div>
+				</div>
 				<form class="form-horizontal" id="form-acuerdo-cambio-representante" action="oficios/acuerdo-cambio-representante-legal.php" enctype="multipart/form-data" method="post" target="_blank">
 					<div class="modal-body">
 						<p>Ingrese el número de oficio para generar este documento</p>
-						<label for="" class="control-label">Número de oficio</label >
+						<label for="" class="control-label">Número de oficio</label>
 						<input type="text" class="form-control" id="modal-acuerdo-cambio-representante-oficio" name="oficio" required>
-						<label for="" class="control-label">Acuerdo RVOE Anterior</label >
+						<label for="" class="control-label">Acuerdo RVOE Anterior</label>
 						<input type="text" class="form-control" name="acuerdo_rvoe_anterior" required>
-						<label for="" class="control-label">Fecha del acuerdo de RVOE Anterior</label >
+						<label for="" class="control-label">Fecha del acuerdo de RVOE Anterior</label>
 						<input type="date" class="form-control" name="fecha_anterior" required>
-						<label for="" class="control-label">Nombre completo del representante legal anterior</label >
+						<label for="" class="control-label">Nombre completo del representante legal anterior</label>
 						<input type="text" class="form-control" name="representante_anterior" required>
-						<label for="" class="control-label">Acta Notariada</label >
+						<label for="" class="control-label">Acta Notariada</label>
 						<textarea class="form-control" name="acta_notariada" required maxlength="255"></textarea>
-					</Div>
+					</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
 						<button id="modal-acuerdo-cambio-representante-aceptar" type="button" class="btn btn-danger" data-dismiss="modal">Aceptar</button>
-					</Div>
+					</div>
 					<input type="hidden" id="modal-acuerdo-cambio-representante-solicitud-id" name="id">
-				</forma>
-			</Div>
-		</Div>
-	</Div>
+				</form>
+			</div>
+		</div>
+	</div>
 
-	<!-- JS GOB. MX -->
-	<script src="https://framework-gb.cdn.gob.mx/gobmx.js"></script >
+	<!-- JS GOB.MX -->
+	<script src="https://framework-gb.cdn.gob.mx/gobmx.js"></script>
 	<!-- JS JQUERY -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script >
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/js/bootstrap-select.min.js"></script >
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/js/bootstrap-select.min.js"></script>
 	<!-- SECCION PARA SCRIPTS -->
-	<script src=".. /js/solicitudes-admin.js"></script>
-	<script src=".. /js/documentos.js"></script>
+	<script src="../js/solicitudes-admin.js"></script>
+	<script src="../js/documentos.js"></script>
 
-</cuerpo>
+</body>
 
 </html>
